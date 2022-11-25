b941d2d454f461b793781c931780caf73a62f6c3
MrSick <15951892481@163.com>
2022-11-25 14:53:50 +0800

    Initial commit web init

---
+++ src/main/java/com/example/jenkins/demo/controller/Testcontroller.java
@@ -0,0 +1,11 @@
+package com.example.jenkins.demo.controller;
+
+/**
+ * @author Solang.Zhao (Solang.Zhao(a)gmail.com)
+ * @version 1.0
+ * @website https://www.cnblogs.com/
+ * @date 2022-11-25 14:14
+ * @since 1.0
+ */
+public class Testcontroller {
+}
